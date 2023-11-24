c7c3d10096f899d4858a8b9a72e950714e1fce19
kannyonny <yohannareuben8@gmail.com>
2023-11-24 01:52:39 -0800

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,22 +1,30 @@
 #include "main.h"
 
 /**
- * flip_bits - counts the number of bits to change
- * to get from one number to another
- * @n: first number
- * @m: second number
+ * flip_bits - Counts the number of bits to change
+ *             to get from one number to another.
+ * @n: First number.
+ * @m: Second number.
  *
- * Return: number of bits to change
+ * Return: Number of bits to change.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int a, countbit = 0;
+	int index, countbit = 0;
 	unsigned long int current;
 	unsigned long int exclusive = n ^ m;
 
-	for (a = 63; a >= 0; a--)
+	/* Betty says, always add some space before loops */
+	for (index = 63; index >= 0; index--)
 	{
-		current = exclusive >> a;
+		/* Betty loves clarity, let's make the current bit clear */
+		current = exclusive >> index;
+
+		/* Betty enjoys counting bits, with style */
 		if (current & 1)
 			countbit++;
 	}
+
+	/* Betty appreciates a well-organized and clean code */
+	return (countbit);
+}
